cc95631b57dc22bb4abbf194e68853a2d51ddc4a
Alexander Batischev <eual.jp@gmail.com>
2012-03-21 21:06:53 +0200

    Initial commit

---
+++ about.markdown
@@ -0,0 +1,37 @@
+Hello,
+
+my name is Alexander Batischev. I'm a student at Kiev Polytechnic Institute
+(Ukraine) where I'm studying computer science. I'm fond of listening to rock
+music (Pink Floyd, Nightwish, Manowar and Dream Theater are my favourite bands)
+and reading sci-fi books.
+
+I'm the owner of [PGP key 0x356961a20c8bfd03][pgp]. If you ever [mail me][email], I'd appreciate it if you make a digital signature.
+
+<center style="font-size: small;">~ * ~</center>
+
+I started [blogging](http://debiania.blogspot.com) back in 2008, a few weeks
+after I installed Debian (hence the name). I kept my blog entries as purely
+technical as possible, with rare exclusions for Linus' birthday and the like ☺
+
+At the end of 2011 I suddenly realized that I don't like Blogspot anymore. They
+once had comments system broken for a month, and the other time my code
+listings went all messed up. 
+
+So I just bought a domain name, rented some hosting, mastered basics of
+[Hakyll](http://github.com/jaspervdj/hakyll) and created new Debiania, the site
+you're reading right now.
+
+Goals are the same — keep posting tech-related entries, not only about Debian,
+but also about programming and CS overall. All the entries from the old blog
+are moved to the new one, just to keep it all in one place.
+
+New Debiania has much simpler interface which is mostly inspired by [Edward Z.
+Yang](http://blog.ezyang.com)'s and [James Hague](http://prog21.dadgum.com)'s
+blogs. I also decided to [abandon commenting][comments] and post articles in
+English alongside with Russian ones. Hope it still can be useful to you.
+
+See you!
+
+[pgp]: http://pgp.mit.edu:11371/pks/lookup?op=vindex&search=0x356961A20C8BFD03 "PGP key 0x356961a20c8bfd03 at pgp.mit.edu"
+[email]: <mailto:eual.jp@gmail.com> "eual dot jp at google mail"
+[comments]: /posts/2012.03.21 Why no comments.html "Why no comments are allowed?"
---
+++ debiania.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Prelude hiding (id)
+import Control.Category (id)
+import Control.Arrow ((>>>), (***), arr)
+import Data.Monoid (mempty, mconcat)
+
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+    -- Compress CSS
+    match "css/*" $ do
+        route idRoute
+        compile compressCssCompiler
+
+    -- Images
+    match "images/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    -- Render posts
+    match "posts/*" $ do
+        route $ setExtension ".html"
+        compile $ pageCompiler
+            >>> applyTemplateCompiler "templates/post.html"
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
+
+    -- Render posts list
+    match "posts.html" $ route idRoute
+    create "posts.html" $ constA mempty
+        >>> arr (setField "title" "Archives")
+        >>> requireAllA "posts/*" addPostList
+        >>> applyTemplateCompiler "templates/archives.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    -- Index
+    match "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> arr (setField "title" "Home")
+        >>> requireAllA "posts/*" (id
+                                   *** arr (take 8 . recentFirst)
+                                   >>> addPostList)
+        >>> applyTemplateCompiler "templates/posts.html"
+        >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    -- Render RSS feed
+    match "rss.xml" $ route idRoute
+    create "rss.xml" $
+        requireAll_ "posts/*"
+            >>> mapCompiler (arr $ copyBodyToField "description")
+            >>> renderRss feedConfiguration
+
+    match "about.markdown" $ do
+        route $ setExtension "html"
+        compile $ pageCompiler
+            >>> arr (setField "title" "About")
+            >>> applyTemplateCompiler "templates/about.html"
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
+
+    -- Read templates
+    match "templates/*" $ compile templateCompiler
+
+-- | Auxiliary compiler: generate a post list from a list of given posts, and
+-- add it to the current page under @$posts@
+--
+addPostList :: Compiler (Page String, [Page String]) (Page String)
+addPostList = setFieldA "posts" $
+    arr recentFirst
+        >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
+        >>> arr mconcat
+        >>> arr pageBody
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+    { feedTitle = "Debiania"
+    , feedDescription = "Just another Debian blog"
+    , feedAuthorName = "Alexander Batischev"
+    , feedRoot = "http://debiania.org.ua"
+    }
