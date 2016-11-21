Minoru/blog.tty8.org

commit 3f2cca58128b8f17deb0e25b95d73da15830ea10

Provide a way to use MathJax in posts

debiania.hs
@@ -13,6 +13,8 @@ import Data.Time.Clock (UTCTime)
 import Data.Time.Format (defaultTimeLocale, parseTimeM, formatTime)
 import Network.HTTP.Base (urlEncode)
 import System.FilePath (takeFileName)
+import Text.Pandoc.Options (WriterOptions(writerHTMLMathMethod),
+    HTMLMathMethod(MathJax), def)
 
 import qualified Data.Aeson.Types as A
 import qualified Data.ByteString.Lazy as LBS
@@ -57,6 +59,15 @@ main = hakyllWith config $ do
 
     -- Images and miscellaneous files
     match ( "images/*" .||. "misc/*" .||. "robots.txt" ) $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    -- MathJax stuff
+    match ( "MathJax/MathJax.js" .||.
+            "MathJax/extensions/**" .||.
+            "MathJax/fonts/**" .||.
+            "MathJax/jax/**" .||.
+            "MathJax/localization/**") $ do
         route   idRoute
         compile copyFileCompiler
 
@@ -321,7 +332,11 @@ debianiaCompiler :: Compiler (Item String)
 debianiaCompiler =
       getResourceBody
   >>= withItemBody (go . split "\n\n$break$\n\n")
-  >>= renderPandoc
+  >>= renderPandocWith
+        def
+        -- The empty string is path to mathjax.js. We don't want Pandoc to
+        -- embed it in output for us as we already do that in Hakyll templates.
+        (def { writerHTMLMathMethod = MathJax "" })
 
   where
   go :: [String] -> Compiler String

.gitmodules
@@ -1,0 +1,3 @@
+[submodule "MathJax"]
+	path = MathJax
+	url = https://github.com/mathjax/MathJax

debiania.cabal
@@ -21,6 +21,7 @@ Executable debiania
                  , bytestring
                  , filepath
                  , hakyll == 4.*
+                 , pandoc
                  , text
                  , time
                  , unordered-containers

templates/default.html
@@ -12,6 +12,19 @@
             title="Debiania" href="/feeds/all.rss">
         <link rel="alternate" type="application/atom+xml"
             title="Debiania" href="/feeds/all.atom">
+        $if(enable-mathjax)$
+        <script type="text/x-mathjax-config">
+            MathJax.Hub.Config({
+                jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML", "output/SVG"],
+                extensions: ["tex2jax.js", "MathMenu.js", "MathZoom.js", "AssistiveMML.js"],
+                TeX: {
+                    extensions: ["AMSmath.js", "AMSsymbols.js", "noErrors.js", "noUndefined.js"]
+                }
+            });
+        </script>
+        <script type="text/javascript" async src="/MathJax/MathJax.js">
+        </script>
+        $endif$
     </head>
     <body>
         <header id="blog-header">

commit b29f947eec15cde5502e6fe2dabf0727ab0f5a5a

Re-enable smart rendering and syntax highlighting
(I broke these in 3f2cca58128b8f17deb0e25b95d73da15830ea10.)

debiania.hs
@@ -333,10 +333,10 @@ debianiaCompiler =
       getResourceBody
   >>= withItemBody (go . split "\n\n$break$\n\n")
   >>= renderPandocWith
-        def
+        defaultHakyllReaderOptions
         -- The empty string is path to mathjax.js. We don't want Pandoc to
         -- embed it in output for us as we already do that in Hakyll templates.
-        (def { writerHTMLMathMethod = MathJax "" })
+        (defaultHakyllWriterOptions { writerHTMLMathMethod = MathJax "" })
 
   where
   go :: [String] -> Compiler String